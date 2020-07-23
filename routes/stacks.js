ebbb5dc84f69ae6281976ef8e4d75f321999e7d2
Chris-css <christelle.savoca@gmail.com>
2020-07-23 03:30:23 +0200

    CRUD stack

--- routes/stacks.js
+++ routes/stacks.js
@@ -2,41 +2,39 @@ const express = require("express");
 const connection = require("../config");
 const router = express.Router();
 
-// I want to create a developper
+// I want to create a stack
 router.post("/", (req, res) => {
-    const sql = "INSERT INTO developper SET ?";
+    const sql = "INSERT INTO stack SET ?";
     const formData = req.body;
     connection.query(sql, [formData], (err, result) => {
         if (err) {
-            res.status(500).send("Erreur lors la création d'un développeur");
+            res.status(500).send(err);
         } else {
-            res.status(200).send("Le développeur a bien été créé");
+            res.status(201).send("La technologie a bien été créée");
         }
     });
 });
 
-// I want to get all developper
+// I want to get all stacks
 router.get("/", (req, res) => {
-    const sql = "SELECT * FROM developper";
+    const sql = "SELECT * FROM stack";
     connection.query(sql, (err, result) => {
         if (err) {
-            res.status(500).send(
-                "Erreur lors de la récupération des développeurs"
-            );
+            res.status(500).send(err);
         } else {
             res.status(200).json(result);
         }
     });
 });
 
-// I want to get one developper
+// I want to get one stack
 router.get("/:id", (req, res) => {
     const id = req.params.id;
-    const sql = "SELECT * FROM developper WHERE id = ?";
+    const sql = "SELECT * FROM stack WHERE id = ?";
     connection.query(sql, [id], (err, result) => {
         if (err) {
             res.status(500).send(
-                "Erreur lors de la récupération d'un développeur"
+                "Erreur lors de la récupération d'une technologie"
             );
         } else {
             res.status(200).json(result);
@@ -44,29 +42,33 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// I want to update one developper
+// I want to update one stack
 router.put("/:id", (req, res) => {
-    const sql = "UPDATE developper SET ? WHERE id = ?";
+    const sql = "UPDATE stack SET ? WHERE id = ?";
     const body = req.body;
     const id = req.params.id;
     connection.query(sql, [body, id], (err, result) => {
         if (err) {
-            res.status(500).send("Le développeur n'a pas été modifié");
+            res.status(500).send(
+                "Erreur lors de la modification d'une technologie"
+            );
         } else {
-            res.status(200).send("Le développeur a bien été modifié");
+            res.status(201).send("La technologie a bien été modifiée");
         }
     });
 });
 
-// I want to delete one developper
+// I want to delete one stack
 router.delete("/:id", (req, res) => {
     const id = req.params.id;
-    const sql = "DELETE FROM developper WHERE id = ?";
-    connection.query(sql, [id], (req, res) => {
+    const sql = "DELETE FROM stack WHERE id = ?";
+    connection.query(sql, [id], (err, result) => {
         if (err) {
-            res.status(500).send(err);
+            res.status(500).send(
+                "Erreur lors de la suppression d'une technologie"
+            );
         } else {
-            res.status(200).send("Le développeur a bien été supprimé");
+            res.status(200).send("La technologie a bien été supprimée");
         }
     });
 });
